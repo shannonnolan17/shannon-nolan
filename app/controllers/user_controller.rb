b21e2b1d2259d371db4226f7ef5dbab7d45e560e
Shannon Nolan <shannonnolan17@gmail.com>
2017-12-19 12:44:03 -0500

    Add devise views

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,20 @@
 class UserController < ApplicationController
+  def new
+    erb :'views/users/sessions/new.html'
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to '/'
+    else
+      @errors = @user.errors.full_messages
+    end
+
+  end
+
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
 end
